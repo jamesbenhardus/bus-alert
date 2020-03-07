3e703642c3f3eb780fe57119ed8d64e8fd9b7382
James Benhardus <james.benhardus@gmail.com>
2020-03-07 10:20:58 -0600

    error handling

--- src/app/metro-transit.service.ts
+++ src/app/metro-transit.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { from } from 'rxjs';
+import { from, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -16,6 +17,11 @@ export class MetroTransitService {
   getStopInfo(stop) {
     const url = this.apiBase + stop + '?format=json';
     // return this.http.get(url, { headers: this.headers });
-    return from(fetch(url).then(response => response.json()));
+    return from(fetch(url).then(response => response.json())).pipe(
+      catchError(err => {
+        console.error(err);
+        return of([{ DepartureText: 'Error' }]);
+      })
+    );
   }
 }
